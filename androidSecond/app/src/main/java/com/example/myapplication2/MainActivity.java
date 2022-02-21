76d479a8d05339732d2ad349d0e95f022c5601da
heojieun <gjekdmlwl@naver.com>
2022-02-21 10:49:40 +0900

    이미지 클릭시 Toast 띄우는 두가지 방법

--- androidSecond/app/src/main/java/com/example/myapplication2/MainActivity.java
+++ androidSecond/app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView ibtn = findViewById(R.id.imageView2);
+        ibtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"나는 이미지입니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onStarClicked(View view) {
+
+        Toast.makeText(this,"나는 별입니다.", Toast.LENGTH_SHORT).show();
+
     }
 }
