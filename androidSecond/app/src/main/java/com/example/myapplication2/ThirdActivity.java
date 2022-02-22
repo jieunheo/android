37718dc6a197f91e2d8bf390a32177a9fe637cb3
heojieun <gjekdmlwl@naver.com>
2022-02-22 09:37:45 +0900

    세번째 화면에서 첫번쨰, 두번째 화면으로 이동하는 버튼 만들기

--- androidSecond/app/src/main/java/com/example/myapplication2/ThirdActivity.java
+++ androidSecond/app/src/main/java/com/example/myapplication2/ThirdActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+    }
+
+    public void onButtonClick(View view) {
+        
+        //두번째 화면으로 가기
+        Intent myIntent = new Intent(getApplicationContext(), SubActivity.class);
+        startActivity(myIntent);
+        
+    }
+
+    public void onButton2Click(View view) {
+
+        //첫번째 화면으로 가기
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(myIntent);
+        
     }
 }
